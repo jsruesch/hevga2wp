9ad1a6c0453bbf5760fc6b2eff6f8dba3bdeef21
Jake Ruesch <jsruesch@gmail.com>
2016-10-20 07:28:32 -0500

    Import Stylesheets

--- header.php
+++ header.php
@@ -2,13 +2,10 @@
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-
 <head profile="http://gmpg.org/xfn/11">
-	
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-	
 	<?php if (is_search()) { ?>
-	   <meta name="robots" content="noindex, nofollow" /> 
+	   <meta name="robots" content="noindex, nofollow" />
 	<?php } ?>
 
 	<title>
@@ -31,28 +28,176 @@
 		         echo ' - page '. $paged; }
 		   ?>
 	</title>
-	
-	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-	
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
-	
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 
+  <!-- jQuery -->
+  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <!-- Bootstrap -->
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css?family=Asap:400,700" rel="stylesheet">
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+  <!-- Institution Accordion Setup -->
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <!-- Linkable TR -->
+  <script type="text/javascript">
+    function DoNav(url)
+    {
+       document.location.href = url;
+    }
+  </script>
+  <script>
+        $(function() {
+          $( "#accordion_usa" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_canada" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_uk" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_australia" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_ireland" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_france" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_turkey" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_singapore" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_china" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_japan" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_guatemala" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_mexico" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_venezuela" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_brazil" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_scotland" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_sweden" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_finland" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_slovakia" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_voting_members" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+    $( "#accordion_non_voting_members" ).accordion({
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+          });
+        });
+  </script>
+  <!-- END: Institution Accordion Setup -->
+
 	<?php wp_head(); ?>
-	
+
 </head>
 
 <body <?php body_class(); ?>>
-	
-<div id="header">
-	<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
-	<div class="description"><?php bloginfo('description'); ?></div>
-</div>
 
+  <!--
+    BEGIN: Navigation
+  -->
+    <div class="row">
+    <nav class="navbar">
+      <nav class="navbar-inverse">
+        <div class="container-fluid bg-black">
+          <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php"><img src="img/nav-logo.png"/></a>
+          </div>
 
-
-
-
-
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="about.php">About</a></li>
+              <li><a href="reports.php">Reports</a></li>
+              <li><a href="news.php">News</a></li>
+              <li><a href="#">DC Meeting</a></li>
+              <li><a href="#">Contact</a></li>
+              <li class="bg-yellow"><a href="#"><span  class="text-black">Join Us</span></a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container -->
+        </div><!-- /.container-fluid -->
+      </nav>
+    </div>
+  <!-- END: Navigation -->
