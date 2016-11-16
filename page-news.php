1babd78664b857afb385e1f9e0b59e3077e02ee3
Jake Ruesch <jsruesch@gmail.com>
2016-11-16 07:20:13 -0600

    Begin news page rework

--- page-news.php
+++ page-news.php
@@ -12,10 +12,61 @@
 -->
   <div class="container-fluid mc">
     <div class="container">
+      <div class="row">
+
+
+        <!-- Featured -->
+        <div class="col-sm-9">
+          <!-- Press Kit -->
+          <div class="row pad15 bg-red">
+            <h2 class="text-white">Featured</h2>
+          </div>
+          <div class="row bg-lgrey pad15 mar20-bot">
+          </div><!-- Press Kit -->
+        </div>
+
+        <!-- Sidebar -->
+        <div class="col-sm-3">
+          <!-- Section Buttons -->
+          <div class="row pad15 bg-dblue">
+            <h4 class="text-center text-white">News</h4>
+          </div>
+          <div class="row pad15 bg-lblue">
+            <h4 class="text-center text-white">Press Releases</h4>
+          </div>
+          <div class="row pad15 bg-green mar20-bot">
+            <h4 class="text-center text-white">Newsletters</h4>
+          </div><!-- Section Buttons -->
+          <!-- Media Contact -->
+          <div class="row pad15 bg-black">
+            <h4 class="text-white">Media Contact</h4>
+          </div>
+          <div class="row bg-lgrey pad15 mar20-bot">
+          </div><!-- Media Contact -->
+          <!-- Press Kit -->
+          <div class="row pad15 bg-black">
+            <h4 class="text-white">Press Kit</h4>
+          </div>
+          <div class="row bg-lgrey pad15 mar20-bot">
+          </div><!-- Press Kit -->
+          <!-- Latest Tweets -->
+          <div class="row pad15 bg-lblue">
+            <h4 class="text-white"><i class="fa fa-twitter-square text-white" aria-hidden="true"></i> Latest Tweets</h4>
+          </div>
+          <div class="row mar20-bot">
+            <a class="twitter-timeline" data-height="550" href="https://twitter.com/HigherEdGames">Tweets by HigherEdGames</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+          </div><!-- Latest Tweets -->
+        </div>
+        <!-- Sidebar -->
+
+      </div><!-- Featured -->
+
+
+
       <div class="row pad15 bg-red">
         <h2 class="text-white">Recent News and Events</h2>
       </div>
-      <!-- Bio -->
+      <!-- Articles -->
       <div class="row bg-lgrey pad15 mar20-bot">
         <table class="col-sm-12">
         <?php query_posts('post_type=news');?>
@@ -52,7 +103,7 @@
                 <h2>Nothing found</h2>
               <?php endif; ?>
           </table>
-      </div>
+      </div><!-- End Articles -->
     </div>
   </div>
 <!-- END: Main Content -->
