0326a126204ba890f2021c08f705e43cdf8875e7
Jake Ruesch <jsruesch@gmail.com>
2016-12-06 07:16:45 -0600

    Change nav hover color and add template to 404 page

--- 404.php
+++ 404.php
@@ -1,7 +1,17 @@
 <?php get_header(); ?>
 
-	<h2>Error 404 - Page Not Found</h2>
-
-<?php get_sidebar(); ?>
+<!--
+  BEGIN: Main Content
+-->
+  <div class="container-fluid mc">
+    <div class="container">
+      <div class="row pad15 bg-black">
+        <h2 class="text-white">Oops!</h2>
+      </div>
+      <div class="row bg-lgrey pad15 mar20-bot">
+				<h3 class="text-center">We're sorry. The page you're looking for can't be found.</h3>
+			</div>
+		</div>
+</div><!-- END: Main Content -->
 
 <?php get_footer(); ?>
