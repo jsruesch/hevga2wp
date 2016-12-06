d684ddc8d0e4c1f6ca976e3bfbe025046fd928b9
Jake Ruesch <jsruesch@gmail.com>
2016-12-06 08:41:15 -0600

    Change apply to have register form instead

--- page-membership.php
+++ page-membership.php
@@ -71,7 +71,7 @@
       </div>
       <div class="row bg-lgrey pad15 mar20-bot">
         <div class="col-sm-6 col-sm-offset-3">
-          <?php echo do_shortcode("[ninja_form id=2]"); ?>
+          <?php echo do_shortcode("[ultimatemember form_id=21]"); ?>
         </div>
       </div>
 
