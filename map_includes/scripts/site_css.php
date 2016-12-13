8fcf78deb28e54fed9c5fead2ec53d6b51ae945c
Jake Ruesch <jsruesch@gmail.com>
2016-12-13 07:52:14 -0600

    Remove interactive map file test

--- map_includes/scripts/site_css.php
+++
@@ -1,7 +0,0 @@
-<?
-	header("Content-type: text/css", true);
-	include("../shell_vars.php");
-	include("../css/css_fonts.php");
-	include("../css/css_html_tags.php");
-	include("../css/css_custom.php");
-?>
