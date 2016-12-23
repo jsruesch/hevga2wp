e97085c1e35b81377be9bc9a7dfeb2283f3acffa
Jake Ruesch <jsruesch@gmail.com>
2016-12-23 12:15:28 -0600

    Add WC to functions.php

--- functions.php
+++ functions.php
@@ -33,4 +33,10 @@
 	add_theme_support( 'post-thumbnails' );
 
 	add_theme_support( 'title-tag' );
+
+	//woocommerce
+	add_action( 'after_setup_theme', 'woocommerce_support' );
+	function woocommerce_support() {
+	    add_theme_support( 'woocommerce' );
+	}
 ?>
