ebb89e245f8ece2047e96179d9d0b20850aafe17
jsruesch <jake@gofarwell.com>
2016-12-28 10:12:59 -0600

    Redirect login to membership

--- header.php
+++ header.php
@@ -178,7 +178,7 @@
               <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="text-yellow">Account</span> <span class="caret text-yellow"></span></a>
                   <ul class="dropdown-menu">
-                      <li><a href="/hevga2wp/login"><span class="text-white"><h5>Login</h5></span></a></li>
+                      <li><a href="/hevga2wp/membership"><span class="text-white"><h5>Login</h5></span></a></li>
                       <li><a href="/hevga2wp/logout"><span class="text-white"><h5>Log Out</h5></span></a></li>
                       <li><a href="/hevga2wp/user"><span class="text-white"><h5>Profile</h5></span></a></li>
                       <!--<li role="separator" class="divider"></li>-->
