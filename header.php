9e8c8ed2ad2856c52d0470ed225c2d92d26fba80
jsruesch <jake@gofarwell.com>
2016-12-28 15:45:07 -0600

    Hide profile link if not logged in

--- header.php
+++ header.php
@@ -180,7 +180,9 @@
                   <ul class="dropdown-menu">
                       <li><a href="/hevga2wp/login"><span class="text-white"><h5>Login</h5></span></a></li>
                       <li><a href="/hevga2wp/logout"><span class="text-white"><h5>Log Out</h5></span></a></li>
-                      <li><a href="/hevga2wp/user"><span class="text-white"><h5>Profile</h5></span></a></li>
+
+                      <?php echo do_shortcode('[hide for="!logged"] <li><a href="/hevga2wp/user"><span class="text-white"><h5>Profile</h5></span></a></li> [/hide]'); ?>
+
                       <!--<li role="separator" class="divider"></li>-->
                   </ul>
               </li>
