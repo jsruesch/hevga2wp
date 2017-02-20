3456ee44d6a5dd43d50ed08269f441be26e5aaa7
jsruesch <jsruesch@gmail.com>
2017-02-20 15:19:58 -0600

    Add Katie

---
+++ fellows/fellow-katie-salen-tekinbas.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ Template Name: Fellow Bio Katie Salen Tekinbas
+*/
+?>
+
+
+<?php get_header(); ?>
+
+<!--
+  BEGIN: Main Content
+-->
+  <div class="container-fluid mc">
+    <div class="container">
+      <div class="col-md-3">
+        <div class="row pad20 bg-green">
+          <div class="row">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
+          </div>
+          <div class="row mar15-top">
+            <h3 class="text-white text-center">Katie Salen Tekinbas</h3>
+            <p class="text-center"><strong>Connected Camps</strong></p>
+          </div>
+        </div>
+        <a href="mailto:katie.salen@gmail.com" target="_blank">
+          <div class="row bg-dgreen pad10-top pad10-bot">
+            <p class="text-white text-center">
+              <i class="fa fa-envelope" aria-hidden="true"></i> Email Katie
+            </p>
+          </div>
+        </a>
+      </div>
+      <!-- Bio -->
+      <div class="col-md-9 bg-lgrey pad15 mar20-bot">
+        <p>
+          Katie Salen Tekinbas is a game designer, co-founder and Chief Designer of Connected Camps, an online learning platform powered by youth Minecraft experts. She is founding Executive Director of Institute of Play, an education nonprofit organization focused on games and learning, and has held tenured faculty positions at Parsons the New School for Design, University of Texas at Austin, and most recently, DePaul University where she taught game design and development. Katie led the team that founded Quest to Learn in 2009, a 6-12th grade public school in New York City. She has published widely and is co-author of Rules of Play, The Game Design Reader, Quest to Learn: Growing a School for Digital Kids, and editor of The Ecology of Games, all from MIT Press. She has worked as a game designer for over 12 years and is a former co-editor of The International Journal of Learning and Media. Katie was an early advocate of the then-hidden world Salen Tekinbas has received grants from the Gates foundation, the MacArthur Foundation, the Carnegie Corporation of New York, the National Science Foundation, the Woodrow Wilson Foundation, the Robert Woods Johnson Foundation, the Margulf Foundation, and Intel, and has given keynotes and featured talks at conferences and business events around the globe. Her non-profit ran an afterschool program focused on immersing middle school youth in game design, technology, and global collaborations via physical and mobile computing expertise, and she once co-designed a karaoke ice cream truck driven by a squirrel. Katie has been involved in the design of slow games, online games, mobile games, and big games in both the commercial and independent games sectors.
+        </p>
+      </div>
+    </div>
+<!-- END: Main Content -->
+
+<?php get_footer(); ?>
--- page-fellows.php
+++ page-fellows.php
@@ -126,6 +126,18 @@
               </div>
             </div>
           </a><!-- END: Mia Consalvo -->
+          <!-- Katie Salen Tekinbas -->
+          <a href="/hevga2wp/fellow-mia-consalvo/">
+            <div class="col-sm-6 col-md-4 pad20 ec-profile">
+              <div class="row">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
+              </div>
+              <div class="row mar15-top">
+                <h4 class="text-dblue text-center">Katie Salen Tekinbas</h4>
+                <p class="text-center"><strong>Connected Camps</strong></p>
+              </div>
+            </div>
+          </a><!-- END: Katie Salen Tekinbas -->
         </div><!-- /.row -->
       </div><!-- END: Fellows -->
     </div>
