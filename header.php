41604fd58f78298a98828cb10e6cf1a13a3193f4
jsruesch <jsruesch@gmail.com>
2017-02-20 13:56:37 -0600

    Add fellows page

--- header.php
+++ header.php
@@ -169,7 +169,7 @@
             <ul class="nav navbar-nav navbar-right">
               <li><a href="/hevga2wp/about" class="text-white">About</a></li>
               <li><a href="/2wp/membership" class="text-white">Membership</a></li>
-              <li><a href="/2wp/fellowship" class="text-white">Fellowship</a></li>
+              <li><a href="/2wp/fellows" class="text-white">Fellows</a></li>
               <!-- <li><a href="/hevga2wp/members" class="text-white">Members</a></li> -->
               <li><a href="/hevga2wp/reports" class="text-white">Reports</a></li>
               <li><a href="/hevga2wp/news" class="text-white">News</a></li>
