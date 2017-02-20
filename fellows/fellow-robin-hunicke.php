94d96a786f4b0d53bc79b89b31ee0466f710402b
jsruesch <jsruesch@gmail.com>
2017-02-20 15:29:04 -0600

    Add Robin

---
+++ fellows/fellow-robin-hunicke.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ Template Name: Fellow Bio Robin Hunicke
+*/
+?>
+
+
+<?php get_header(); ?>
+
+<!--
+  BEGIN: Main Content
+-->
+  <div class="container-fluid mc">
+    <div class="container">
+      <div class="col-md-3">
+        <div class="row pad20 bg-green">
+          <div class="row">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
+          </div>
+          <div class="row mar15-top">
+            <h3 class="text-white text-center">Robin Hunicke</h3>
+            <p class="text-center"><strong>Funomena</strong></p>
+          </div>
+        </div>
+        <a href="mailto:robin@funomena.com" target="_blank">
+          <div class="row bg-dgreen pad10-top pad10-bot">
+            <p class="text-white text-center">
+              <i class="fa fa-envelope" aria-hidden="true"></i> Email Robin
+            </p>
+          </div>
+        </a>
+      </div>
+      <!-- Bio -->
+      <div class="col-md-9 bg-lgrey pad15 mar20-bot">
+        <p>
+          Robin Hunicke is the Co-Founder & CEO of the independent game studio Funomena, which creates experimental games for Console, PC, VR & AR platforms in the heart of downtown San Francisco. A game designer and producer by training, she has a background in Computer Science, Art and applied Game Studies. She has been designing, making and teaching about games for over 12 years (Journey, Boom Blox, MySims, TheSims2).<br /><br />
+          Robin is an outspoken evangelist for diversity, and has dedicated the second half of her career to broadening the participation of women and under-represented minorities within the Arts, Entertainment & Technology communities. To that end, she joined UCSC's Arts Division as Associate Professor as Director of the Art, Games & Playable Media BA in 2014. In 2015 she joined the Digital Arts and New Media MFA program faculty​, and founded ​the Ludo Lab ​Center​ for Games, Play & SocialPractice​. Along with her colleagues throughout the UC system, Robin works to provide students with opportunities to explore how they can impact society via creative, technological, social & entrepreneurial practices. <br /><br />
+          In addition to her commercial and educational work, Robin ​curates the annual Experimental Gameplay Workshop, sits on ​the PlaySFMOMA​, FDG​ & ​VR​DC advisory boards​, and​ volunteer​s ​in support of initiatives such as: ​the Lexus Design Award, Girls Make Games, Anita Borg Foundation,​ ​Google Made with Code​, as well as the Higher Education Video Game A​lliance and ​the CS For Al​l​ initiative.
+        </p>
+      </div>
+    </div>
+<!-- END: Main Content -->
+
+<?php get_footer(); ?>
--- page-fellows.php
+++ page-fellows.php
@@ -150,6 +150,18 @@
               </div>
             </div>
           </a><!-- END: R. Michael Young -->
+          <!-- Robin Hunicke -->
+          <a href="/hevga2wp/fellow-robin-hunicke/">
+            <div class="col-sm-6 col-md-4 pad20 ec-profile">
+              <div class="row">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
+              </div>
+              <div class="row mar15-top">
+                <h4 class="text-dblue text-center">Robin Hunicke</h4>
+                <p class="text-center"><strong>Funomena</strong></p>
+              </div>
+            </div>
+          </a><!-- END: Robin Hunicke -->
         </div><!-- /.row -->
       </div><!-- END: Fellows -->
     </div>
