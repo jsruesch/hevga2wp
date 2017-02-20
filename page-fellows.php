ba2cb1ff6e6802c9b10afe542ea8699575573439
jsruesch <jsruesch@gmail.com>
2017-02-20 15:54:31 -0600

    Add portrait for tracy

--- page-fellows.php
+++ page-fellows.php
@@ -82,7 +82,7 @@
           <a href="/hevga2wp/fellow-tracy-fullerton/">
             <div class="col-sm-6 col-md-4 pad20 ec-profile">
               <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_tracy.png" class="center-block"/>
               </div>
               <div class="row mar15-top">
                 <h4 class="text-dblue text-center">Tracy Fullerton</h4>
