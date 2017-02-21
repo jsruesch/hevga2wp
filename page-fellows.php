1c990a13ef47709163b0d3f5a52220807d34c1ae
jsruesch <jsruesch@gmail.com>
2017-02-21 14:11:50 -0600

    Add Annika portrait

--- page-fellows.php
+++ page-fellows.php
@@ -94,7 +94,7 @@
           <a href="/hevga2wp/fellow-annika-waern/">
             <div class="col-sm-6 col-md-4 pad20 ec-profile">
               <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_annika.png" class="center-block"/>
               </div>
               <div class="row mar15-top">
                 <h4 class="text-dblue text-center">Annika Waern</h4>
