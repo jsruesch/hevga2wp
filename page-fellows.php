09080292f8d13fa7e32e078619b233b086bd1a7f
jsruesch <jsruesch@gmail.com>
2017-02-21 14:17:02 -0600

    Add Katie portrait

--- page-fellows.php
+++ page-fellows.php
@@ -130,7 +130,7 @@
           <a href="/hevga2wp/fellow-katie-salen-tekinbas/">
             <div class="col-sm-6 col-md-4 pad20 ec-profile">
               <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_katie.png" class="center-block"/>
               </div>
               <div class="row mar15-top">
                 <h4 class="text-dblue text-center">Katie Salen Tekinbas</h4>
