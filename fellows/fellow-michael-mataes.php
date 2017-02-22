5d3129e4b93486cbe0c36b4bd41ca4131c87f840
jsruesch <jsruesch@gmail.com>
2017-02-22 14:28:52 -0600

    Add Michael Mataes temp

---
+++ fellows/fellow-michael-mataes.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ Template Name: Fellow Bio Michael Mataes
+*/
+?>
+
+
+<?php get_header(); ?>
+
+<!--
+  BEGIN: Main Content
+-->
+  <div class="container-fluid mc">
+    <div class="container">
+      <!-- Andrew -->
+      <div class="col-md-3">
+        <div class="row pad20 bg-green">
+          <div class="row">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
+          </div>
+          <div class="row mar15-top">
+            <h3 class="text-white text-center">Michael Mataes</h3>
+            <p class="text-center"><strong>N/A</strong></p>
+          </div>
+        </div>
+        <!-- <a href="mailto:michaelm@soe.ucsc.edu" target="_blank">
+          <div class="row bg-dgreen pad10-top pad10-bot">
+            <p class="text-white text-center">
+              <i class="fa fa-envelope" aria-hidden="true"></i> Email Michael
+            </p>
+          </div>
+        </a> -->
+      </div><!-- /Andrew -->
+      <!-- Bio -->
+      <div class="col-md-9 bg-lgrey pad15 mar20-bot">
+        <p>
+          n/a
+        </p>
+      </div>
+    </div>
+  </div>
+<!-- END: Main Content -->
+
+<?php get_footer(); ?>
--- page-fellows.php
+++ page-fellows.php
@@ -198,6 +198,18 @@
               </div>
             </div>
           </a><!-- END: Colleen Macklin -->
+          <!-- Michael Mataes -->
+          <a href="/hevga2wp/fellow-michael-mataes/">
+            <div class="col-sm-6 col-md-4 pad20 ec-profile">
+              <div class="row">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
+              </div>
+              <div class="row mar15-top">
+                <h4 class="text-dblue text-center">Michael Mataes</h4>
+                <p class="text-center"><strong>n/a</strong></p>
+              </div>
+            </div>
+          </a><!-- END: Michael Mataes -->
           <!-- Frans Mäyrä -->
           <a href="/hevga2wp/fellow-frans-mayra/">
             <div class="col-sm-6 col-md-4 pad20 ec-profile">
