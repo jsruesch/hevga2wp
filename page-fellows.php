c09ccbab2f219985f7d7ea3f6e30d44588ec0f9b
jsruesch <jsruesch@gmail.com>
2017-02-22 10:06:56 -0600

    Alphabetize fellows

--- page-fellows.php
+++ page-fellows.php
@@ -18,102 +18,18 @@
           <h2 class="text-white">HEVGA FELLOWS</h2>
         </div>
         <div class="row bg-lgrey">
-          <!-- Eric Klopfer -->
-          <a href="/hevga2wp/fellow-eric-klopfer/">
+          <!-- Ian Bogost -->
+          <a href="/hevga2wp/fellow-ian-bogost/">
             <div class="col-sm-6 col-md-4 pad20 ec-profile">
               <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_eric.png" class="center-block"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_ian.png" class="center-block"/>
               </div>
               <div class="row mar15-top">
-                <h4 class="text-dblue text-center">Eric Klopfer</h4>
-                <p class="text-center"><strong>MIT</strong></p>
+                <h4 class="text-dblue text-center">Ian Bogost</h4>
+                <p class="text-center"><strong>Georgia Institute of Technology</strong></p>
               </div>
             </div>
-          </a><!-- END: Eric Klopfer -->
-          <!-- John Sharp -->
-          <a href="/hevga2wp/fellow-john-sharp/">
-            <div class="col-sm-6 col-md-4 pad20 ec-profile">
-              <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
-              </div>
-              <div class="row mar15-top">
-                <h4 class="text-dblue text-center">John Sharp</h4>
-                <p class="text-center"><strong>The New School</strong></p>
-              </div>
-            </div>
-          </a><!-- END: John Sharp -->
-          <!-- Zoran Popovic -->
-          <a href="/hevga2wp/fellow-zoran-popovic/">
-            <div class="col-sm-6 col-md-4 pad20 ec-profile">
-              <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
-              </div>
-              <div class="row mar15-top">
-                <h4 class="text-dblue text-center">Zoran Popovic</h4>
-                <p class="text-center"><strong>University of Washington</strong></p>
-              </div>
-            </div>
-          </a><!-- END: Zoran Popovic -->
-          <!-- Magy Seif El-Nasr -->
-          <a href="/hevga2wp/fellow-magy-seif-el-nasr/">
-            <div class="col-sm-6 col-md-4 pad20 ec-profile">
-              <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_magy.png" class="center-block"/>
-              </div>
-              <div class="row mar15-top">
-                <h4 class="text-dblue text-center">Magy Seif El-Nasr</h4>
-                <p class="text-center"><strong>Northeastern University</strong></p>
-              </div>
-            </div>
-          </a><!-- END: Magy Seif El-Nasr -->
-          <!-- Frans Mäyrä -->
-          <a href="/hevga2wp/fellow-frans-mayra/">
-            <div class="col-sm-6 col-md-4 pad20 ec-profile">
-              <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
-              </div>
-              <div class="row mar15-top">
-                <h4 class="text-dblue text-center">Frans Mäyrä</h4>
-                <p class="text-center"><strong>University of Tampere</strong></p>
-              </div>
-            </div>
-          </a><!-- END: Frans Mäyrä -->
-          <!-- Tracy Fullerton -->
-          <a href="/hevga2wp/fellow-tracy-fullerton/">
-            <div class="col-sm-6 col-md-4 pad20 ec-profile">
-              <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_tracy.png" class="center-block"/>
-              </div>
-              <div class="row mar15-top">
-                <h4 class="text-dblue text-center">Tracy Fullerton</h4>
-                <p class="text-center"><strong>USC Games</strong></p>
-              </div>
-            </div>
-          </a><!-- END: Tracy Fullerton -->
-          <!-- Annika Waern -->
-          <a href="/hevga2wp/fellow-annika-waern/">
-            <div class="col-sm-6 col-md-4 pad20 ec-profile">
-              <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_annika.png" class="center-block"/>
-              </div>
-              <div class="row mar15-top">
-                <h4 class="text-dblue text-center">Annika Waern</h4>
-                <p class="text-center"><strong>Uppsala University</strong></p>
-              </div>
-            </div>
-          </a><!-- END: Annika Waern -->
-          <!-- Brenda Romero -->
-          <a href="/hevga2wp/fellow-brenda-romero/">
-            <div class="col-sm-6 col-md-4 pad20 ec-profile">
-              <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_brenda.png" class="center-block"/>
-              </div>
-              <div class="row mar15-top">
-                <h4 class="text-dblue text-center">Brenda Romero</h4>
-                <p class="text-center"><strong>University of Limerick</strong></p>
-              </div>
-            </div>
-          </a><!-- END: Brenda Romero -->
+          </a><!-- END: Ian Bogost -->
           <!-- Mia Consalvo -->
           <a href="/hevga2wp/fellow-mia-consalvo/">
             <div class="col-sm-6 col-md-4 pad20 ec-profile">
@@ -126,30 +42,18 @@
               </div>
             </div>
           </a><!-- END: Mia Consalvo -->
-          <!-- Katie Salen Tekinbas -->
-          <a href="/hevga2wp/fellow-katie-salen-tekinbas/">
+          <!-- Tracy Fullerton -->
+          <a href="/hevga2wp/fellow-tracy-fullerton/">
             <div class="col-sm-6 col-md-4 pad20 ec-profile">
               <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_katie.png" class="center-block"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_tracy.png" class="center-block"/>
               </div>
               <div class="row mar15-top">
-                <h4 class="text-dblue text-center">Katie Salen Tekinbas</h4>
-                <p class="text-center"><strong>Connected Camps</strong></p>
+                <h4 class="text-dblue text-center">Tracy Fullerton</h4>
+                <p class="text-center"><strong>USC Games</strong></p>
               </div>
             </div>
-          </a><!-- END: Katie Salen Tekinbas -->
-          <!-- R. Michael Young -->
-          <a href="/hevga2wp/fellow-r-michael-young/">
-            <div class="col-sm-6 col-md-4 pad20 ec-profile">
-              <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_michael.png" class="center-block"/>
-              </div>
-              <div class="row mar15-top">
-                <h4 class="text-dblue text-center">R. Michael Young</h4>
-                <p class="text-center"><strong>University of Utah</strong></p>
-              </div>
-            </div>
-          </a><!-- END: R. Michael Young -->
+          </a><!-- END: Tracy Fullerton -->
           <!-- Robin Hunicke -->
           <a href="/hevga2wp/fellow-robin-hunicke/">
             <div class="col-sm-6 col-md-4 pad20 ec-profile">
@@ -174,18 +78,30 @@
               </div>
             </div>
           </a><!-- END: Henry Jenkins -->
-          <!-- Eric Zimmerman -->
-          <a href="/hevga2wp/fellow-eric-zimmerman/">
+          <!-- Eric Klopfer -->
+          <a href="/hevga2wp/fellow-eric-klopfer/">
             <div class="col-sm-6 col-md-4 pad20 ec-profile">
               <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_eric_z.png" class="center-block"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_eric.png" class="center-block"/>
               </div>
               <div class="row mar15-top">
-                <h4 class="text-dblue text-center">Eric Zimmerman</h4>
-                <p class="text-center"><strong>NYU</strong></p>
+                <h4 class="text-dblue text-center">Eric Klopfer</h4>
+                <p class="text-center"><strong>MIT</strong></p>
               </div>
             </div>
-          </a><!-- END: Eric Zimmerman -->
+          </a><!-- END: Eric Klopfer -->
+          <!-- Frans Mäyrä -->
+          <a href="/hevga2wp/fellow-frans-mayra/">
+            <div class="col-sm-6 col-md-4 pad20 ec-profile">
+              <div class="row">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
+              </div>
+              <div class="row mar15-top">
+                <h4 class="text-dblue text-center">Frans Mäyrä</h4>
+                <p class="text-center"><strong>University of Tampere</strong></p>
+              </div>
+            </div>
+          </a><!-- END: Frans Mäyrä -->
           <!-- Janet H. Murray -->
           <a href="/hevga2wp/fellow-janet-h-murray/">
             <div class="col-sm-6 col-md-4 pad20 ec-profile">
@@ -210,18 +126,102 @@
               </div>
             </div>
           </a><!-- END: Scot Osterweil -->
-          <!-- Ian Bogost -->
-          <a href="/hevga2wp/fellow-ian-bogost/">
+          <!-- Zoran Popovic -->
+          <a href="/hevga2wp/fellow-zoran-popovic/">
             <div class="col-sm-6 col-md-4 pad20 ec-profile">
               <div class="row">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_ian.png" class="center-block"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
               </div>
               <div class="row mar15-top">
-                <h4 class="text-dblue text-center">Ian Bogost</h4>
-                <p class="text-center"><strong>Georgia Institute of Technology</strong></p>
+                <h4 class="text-dblue text-center">Zoran Popovic</h4>
+                <p class="text-center"><strong>University of Washington</strong></p>
               </div>
             </div>
-          </a><!-- END: Ian Bogost -->
+          </a><!-- END: Zoran Popovic -->
+          <!-- Brenda Romero -->
+          <a href="/hevga2wp/fellow-brenda-romero/">
+            <div class="col-sm-6 col-md-4 pad20 ec-profile">
+              <div class="row">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_brenda.png" class="center-block"/>
+              </div>
+              <div class="row mar15-top">
+                <h4 class="text-dblue text-center">Brenda Romero</h4>
+                <p class="text-center"><strong>University of Limerick</strong></p>
+              </div>
+            </div>
+          </a><!-- END: Brenda Romero -->
+          <!-- Magy Seif El-Nasr -->
+          <a href="/hevga2wp/fellow-magy-seif-el-nasr/">
+            <div class="col-sm-6 col-md-4 pad20 ec-profile">
+              <div class="row">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_magy.png" class="center-block"/>
+              </div>
+              <div class="row mar15-top">
+                <h4 class="text-dblue text-center">Magy Seif El-Nasr</h4>
+                <p class="text-center"><strong>Northeastern University</strong></p>
+              </div>
+            </div>
+          </a><!-- END: Magy Seif El-Nasr -->
+          <!-- John Sharp -->
+          <a href="/hevga2wp/fellow-john-sharp/">
+            <div class="col-sm-6 col-md-4 pad20 ec-profile">
+              <div class="row">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
+              </div>
+              <div class="row mar15-top">
+                <h4 class="text-dblue text-center">John Sharp</h4>
+                <p class="text-center"><strong>The New School</strong></p>
+              </div>
+            </div>
+          </a><!-- END: John Sharp -->
+          <!-- Katie Salen Tekinbas -->
+          <a href="/hevga2wp/fellow-katie-salen-tekinbas/">
+            <div class="col-sm-6 col-md-4 pad20 ec-profile">
+              <div class="row">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_katie.png" class="center-block"/>
+              </div>
+              <div class="row mar15-top">
+                <h4 class="text-dblue text-center">Katie Salen Tekinbas</h4>
+                <p class="text-center"><strong>Connected Camps</strong></p>
+              </div>
+            </div>
+          </a><!-- END: Katie Salen Tekinbas -->
+          <!-- Annika Waern -->
+          <a href="/hevga2wp/fellow-annika-waern/">
+            <div class="col-sm-6 col-md-4 pad20 ec-profile">
+              <div class="row">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_annika.png" class="center-block"/>
+              </div>
+              <div class="row mar15-top">
+                <h4 class="text-dblue text-center">Annika Waern</h4>
+                <p class="text-center"><strong>Uppsala University</strong></p>
+              </div>
+            </div>
+          </a><!-- END: Annika Waern -->
+          <!-- R. Michael Young -->
+          <a href="/hevga2wp/fellow-r-michael-young/">
+            <div class="col-sm-6 col-md-4 pad20 ec-profile">
+              <div class="row">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_michael.png" class="center-block"/>
+              </div>
+              <div class="row mar15-top">
+                <h4 class="text-dblue text-center">R. Michael Young</h4>
+                <p class="text-center"><strong>University of Utah</strong></p>
+              </div>
+            </div>
+          </a><!-- END: R. Michael Young -->
+          <!-- Eric Zimmerman -->
+          <a href="/hevga2wp/fellow-eric-zimmerman/">
+            <div class="col-sm-6 col-md-4 pad20 ec-profile">
+              <div class="row">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_eric_z.png" class="center-block"/>
+              </div>
+              <div class="row mar15-top">
+                <h4 class="text-dblue text-center">Eric Zimmerman</h4>
+                <p class="text-center"><strong>NYU</strong></p>
+              </div>
+            </div>
+          </a><!-- END: Eric Zimmerman -->
         </div><!-- /.row -->
       </div><!-- END: Fellows -->
     </div>
