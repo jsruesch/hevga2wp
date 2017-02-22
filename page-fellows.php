40637fe3a8b1492bf417f53d4a088596bd90a434
jsruesch <jsruesch@gmail.com>
2017-02-22 14:31:31 -0600

    Adjust fellow links to point to the right page

--- page-fellows.php
+++ page-fellows.php
@@ -235,7 +235,7 @@
             </div>
           </a><!-- END: Janet H. Murray -->
           <!-- Scot Osterweil -->
-          <a href="/hevga2wp/fellow-janet-h-murray/">
+          <a href="/hevga2wp/fellow-scot-osterweil/">
             <div class="col-sm-6 col-md-4 pad20 ec-profile">
               <div class="row">
                 <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_scot.png" class="center-block"/>
@@ -343,7 +343,7 @@
             </div>
           </a><!-- END: Katie Salen Tekinbas -->
           <!-- Kurt Squire -->
-          <a href="/hevga2wp/fellow-katie-salen-tekinbas/">
+          <a href="/hevga2wp/fellow-kurt-squire/">
             <div class="col-sm-6 col-md-4 pad20 ec-profile">
               <div class="row">
                 <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
