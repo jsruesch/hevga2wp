4bf4b5efedb33a6e1af9a19a9d29edb8a9e631a9
jsruesch <jsruesch@gmail.com>
2017-02-22 10:51:14 -0600

    Add Roger

---
+++ fellows/fellow-roger-altizer.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ Template Name: Fellow Bio Roger Altizer, Jr., Ph.D.
+*/
+?>
+
+
+<?php get_header(); ?>
+
+<!--
+  BEGIN: Main Content
+-->
+  <div class="container-fluid mc">
+    <div class="container">
+      <!-- Andrew -->
+      <div class="col-md-3">
+        <div class="row pad20 bg-green">
+          <div class="row">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_roger.png" class="center-block"/>
+          </div>
+          <div class="row mar15-top">
+            <h3 class="text-white text-center">Roger Altizer, Jr., Ph.D.</h3>
+            <p class="text-center"><strong>University of Utah</strong></p>
+          </div>
+        </div>
+        <a href="mailto:roger.altizer@utah.edu" target="_blank">
+          <div class="row bg-dgreen pad10-top pad10-bot">
+            <p class="text-white text-center">
+              <i class="fa fa-envelope" aria-hidden="true"></i> Email Roger
+            </p>
+          </div>
+        </a>
+      </div><!-- /Andrew -->
+      <!-- Bio -->
+      <div class="col-md-9 bg-lgrey pad15 mar20-bot">
+        <p>
+          Roger Altizer, Jr. is the co-founder of the Entertainment Arts and Engineering, the Director of Digital Medicine for the Center for Medical Innovation, the founding Director of The GApp lab (Therapeutic Games and Apps) and former director of the Center for Interdisciplinary Art and Technology at the University of Utah. Roger earned his Ph.D. in Communication at Utah and specializes in serious and medical games as well as game design and development. His heart and his research focuses on the importance of play and how good design encourages it. Creator of The Design Box, a participatory/inductive design methodology, Roger strives to include users as designers in his work. A former games journalist, he is an internationally recognized speaker who has presented at industry conferences ranging from the Games Developer Conference to the Penny Arcade Expo, and academic conferences including the Digital Games Research Association and Foundations of Digital Games.
+        </p>
+      </div>
+    </div>
+  </div>
+<!-- END: Main Content -->
+
+<?php get_footer(); ?>
--- page-fellows.php
+++ page-fellows.php
@@ -18,6 +18,18 @@
           <h2 class="text-white">HEVGA FELLOWS</h2>
         </div>
         <div class="row bg-lgrey">
+          <!-- Roger Altizer, Jr., Ph.D. -->
+          <a href="/hevga2wp/fellow-roger-altizer/">
+            <div class="col-sm-6 col-md-4 pad20 ec-profile">
+              <div class="row">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_roger.png" class="center-block"/>
+              </div>
+              <div class="row mar15-top">
+                <h4 class="text-dblue text-center">Roger Altizer, Jr., Ph.D.</h4>
+                <p class="text-center"><strong>University of Utah</strong></p>
+              </div>
+            </div>
+          </a><!-- END: Roger Altizer, Jr., Ph.D. -->
           <!-- Ian Bogost -->
           <a href="/hevga2wp/fellow-ian-bogost/">
             <div class="col-sm-6 col-md-4 pad20 ec-profile">
