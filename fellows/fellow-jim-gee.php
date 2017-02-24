b7803ed228bd7ad0068a4fd5c7f4920176678712
jsruesch <jsruesch@gmail.com>
2017-02-24 14:28:04 -0600

    Add Jim Gee portrait

--- fellows/fellow-jim-gee.php
+++ fellows/fellow-jim-gee.php
@@ -16,11 +16,11 @@
       <div class="col-md-3">
         <div class="row pad20 bg-green">
           <div class="row">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_none.gif" class="center-block"/>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/portraits/portrait_jim_g.png" class="center-block"/>
           </div>
           <div class="row mar15-top">
             <h3 class="text-white text-center">Jim Gee</h3>
-            <p class="text-center"><strong>N/A</strong></p>
+            <p class="text-center"><strong>Arizona State University</strong></p>
           </div>
         </div>
         <!-- <a href="mailto:jpgee@asu.edu" target="_blank">
@@ -34,7 +34,7 @@
       <!-- Bio -->
       <div class="col-md-9 bg-lgrey pad15 mar20-bot">
         <p>
-          n/a
+          James Paul Gee is a member of the National Academy of Education.  His book Sociolinguistics and Literacies (1990, Fifth Edition 2015) was one of the founding documents in the formation of the “New Literacy Studies”, an interdisciplinary field devoted to studying language, learning, and literacy in an integrated way in the full range of their cognitive, social, and cultural contexts.  His book An Introduction to Discourse Analysis (1999, Fourth Edition 2014) brings together his work on a methodology for studying communication in its cultural settings, an approach that has been widely influential over the last two decades. Professor Gee’s most recent books deal with video games, language, and learning.  What Video Games Have to Teach Us About Learning and Literacy (2003, Second Edition 2007) argues that good video games are designed to enhance learning through effective learning principles supported by research in the Learning Sciences.  Situated Language and Learning (2004) places video games within an overall theory of learning and literacy and shows how they can help us in thinking about the reform of schools.  Women as Gamers: The Sims and 21st-Century Learning (2010) and Language and Learning in the Digital Age (2011), both written with Elisabeth Hayes, have continued his earlier work on games and learning.  His most recent books are: The Anti-Education Era: Creating Smarter Students through Digital Learning (2013); Unified Discourse Analysis: Language, Reality, Virtual Worlds, and Video Games (2014); Literacy and Education (2014); The Essential James Gee: An Introduction to Discourse Analysis (2015); and Teaching, Learning, Literacy in Our High-Risk High-Tech World: A Framework for Becoming Human (2017).  Professor Gee has published widely in journals in linguistics, psychology, the social sciences, and education.
         </p>
       </div>
     </div>
