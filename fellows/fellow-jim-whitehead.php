8c9414efd13371c4a306e10c634c92216bc2fc0b
jsruesch <jsruesch@gmail.com>
2017-02-24 08:28:52 -0600

    Add Jim Whitehead information

--- fellows/fellow-jim-whitehead.php
+++ fellows/fellow-jim-whitehead.php
@@ -20,10 +20,10 @@
           </div>
           <div class="row mar15-top">
             <h3 class="text-white text-center">Jim Whitehead</h3>
-            <p class="text-center"><strong>N/A</strong></p>
+            <p class="text-center"><strong>University of California, Santa Cruz</strong></p>
           </div>
         </div>
-        <!-- <a href="mailto:Ejw@soe.ucsc.edu " target="_blank">
+        <!-- <a href="mailto:ejw@soe.ucsc.edu" target="_blank">
           <div class="row bg-dgreen pad10-top pad10-bot">
             <p class="text-white text-center">
               <i class="fa fa-envelope" aria-hidden="true"></i> Email Jim
@@ -34,7 +34,7 @@
       <!-- Bio -->
       <div class="col-md-9 bg-lgrey pad15 mar20-bot">
         <p>
-          n/a
+          Jim Whitehead is Professor of Computational Media at the University of California, Santa Cruz. Within computer games, his research interests include procedural content generation and tools where humans and computer generators work collaboratively. Jim is the Founding President of the Society for the Advancement of the Science of Digital Games (SASDG) which operates the yearly Foundations of Digital Games conference. Jim helped launch the UC Santa Cruz computer games program in 2006. Additionally, Jim was a founding organizer of the Procedural Content Generation workshop series, and the Games and Software Engineering workshop series. Jim received his PhD from the University of California, Irvine in Information and Computer Science in 2000, under his advisor, Richard N. Taylor. He has been a Professor at Univ. of California, Santa Cruz since July, 2000, in Computer Science until 2014, and was Chair of Computer Science 2010-2014. He is a founding member of the Department of Computational Media.
         </p>
       </div>
     </div>
