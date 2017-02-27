013423db403ffc15e55bb93125538dac55ef7b85
jsruesch <jsruesch@gmail.com>
2017-02-27 08:55:21 -0600

    Add Jesper Juul bio

--- fellows/fellow-jesper-juul.php
+++ fellows/fellow-jesper-juul.php
@@ -20,7 +20,7 @@
           </div>
           <div class="row mar15-top">
             <h3 class="text-white text-center">Jesper Juul</h3>
-            <p class="text-center"><strong>N/A</strong></p>
+            <p class="text-center"><strong>Royal Danish Academy of Fine Arts, School of Design</strong></p>
           </div>
         </div>
         <!-- <a href="mailto:j@jesperjuul.net " target="_blank">
@@ -34,7 +34,9 @@
       <!-- Bio -->
       <div class="col-md-9 bg-lgrey pad15 mar20-bot">
         <p>
-          n/a
+          Jesper Juul is an influential video game theorist and occasional video game developer. He has been working with video game research since the late 1990's. He is an Associate Professor at the Royal Danish Academy of Fine Arts - School of Design and a Visiting Associate Professor at MIT.<br /><br />
+          He has published three books with MIT Press: Half-Real (2005), A Casual Revolution (2009) and The Art of Failure (2013). He is also a co-editor of the Playful Thinking Series (also on MIT Press).<br /><br />
+          He maintains the blog The Ludologist on video games and other important things. www.jesperjuul.net/ludologist
         </p>
       </div>
     </div>
